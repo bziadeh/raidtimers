f817e0a85d0a1943c4f41ca8fd2a1534ebd41e6f
Brennan <491brennan@gmail.com>
2021-11-13 20:46:38 -0500

    Minor changes to variable names and javadocs

--- src/com/cloth/RaidTimers.java
+++ src/com/cloth/RaidTimers.java
@@ -13,21 +13,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-/**
- * @author Brennan Ziadeh
- * @author https://github.com/bziadeh
- */
 public class RaidTimers extends JavaPlugin {
 
     @Getter private static RaidTimers instance;
+    @Getter private Config localConfig;
+    @Getter private RaidApi api;
 
-    private Config config;
-    private RaidApi api;
-
+    /**
+     * Called when this plugin is enabled. All instantiated classes
+     * must go here instead of the constructor to ensure dependencies are loaded.
+     */
     public void onEnable() {
         instance = this;
-
-        config = Config.load(this);
+        localConfig = Config.load(this);
         api = RaidApi.create();
 
         new RaidListener(this);
@@ -38,8 +36,15 @@ public class RaidTimers extends JavaPlugin {
         Bukkit.getScheduler().runTaskTimer(this, new ShieldRunnable(), 0, 1200);
 
         // todo: prevent faction disband during raids...
+        // todo: time remaining feature?
+        // todo: rewards? kill other members to get items?
+
     }
 
+    /**
+     * Called when this plugin is disabled. Do any necessary cleanup
+     * here to ensure there aren't memory leaks or unneeded tasks still running.
+     */
     public void onDisable() {
         final List<RaidContext> raids = api.getContextList();
         // cleanup before loading raids from flat-file
@@ -59,23 +64,5 @@ public class RaidTimers extends JavaPlugin {
      */
     public static void registerListener(Listener listener) {
         Bukkit.getPluginManager().registerEvents(listener, instance);
-    }
-
-    /**
-     * Gets the Config.
-     *
-     * @return the Config.
-     */
-    public Config getLocalConfig() {
-        return config;
-    }
-
-    /**
-     * Gets the RaidTimers API.
-     *
-     * @return the API.
-     */
-    public RaidApi getApi() {
-        return api == null ? (api = RaidApi.create()) : api;
     }
 }
